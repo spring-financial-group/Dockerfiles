afa657f92320c76428b6b652742fc2bcd22ef4df
AlexKondrat <alexander1996kondratenko@gmail.com>
2022-05-04 11:37:35 +0300

    fix: update trivydb downloader.sh

--- trivydb/downloader.sh
+++ trivydb/downloader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export version=`curl -s https://github.com/aquasecurity/trivy-db/releases/latest |  grep "tag" | sed -n 's/.*href="\([^"]*\).*/\1/p' | cut -d "/" -f8`
+export version=`curl -s https://github.com/aquasecurity/trivy-db/releases |  grep "tag" | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep "tag" | grep "releases" | head -1 | cut -d "/" -f6`
 wget https://github.com/aquasecurity/trivy-db/releases/download/${version}/trivy-offline.db.tgz
 tar -xzvf trivy-offline.db.tgz
