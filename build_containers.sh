780f8f7eaa411c32d13d6b9d3f220e158ec9a8a3
Skisocks <jaa.skeoch@gmail.com>
2023-05-04 09:40:15 +0100

    chore: remove redundant files

--- build_containers.sh
+++
@@ -1,9 +0,0 @@
-#!/busybox/sh
-set -e
-source .jx/variables.sh
-cp /tekton/creds-secrets/tekton-container-registry-auth/.dockerconfigjson /kaniko/.docker/config.json
-
-for image in $(find . -maxdepth 1 -type d | sed 's|^./||' | grep -F -v .); do
-  echo -e "\n------------------ BUILDING CONTAINER $image ------------------\n"
-  /kaniko/executor $KANIKO_FLAGS --cleanup --context=/workspace/source/$image --dockerfile="Dockerfile" --destination=$PUSH_CONTAINER_REGISTRY/$DOCKER_REGISTRY_ORG/$image:$VERSION --no-push --tarPath "$image".tar
-done
